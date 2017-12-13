65947737eaae1c88399fe4c982596a8dcb82aa06
Green Lightning <GreenLightning.git@googlemail.com>
2017-12-13 12:09:36 +0100

    Optimize day 13.

--- day13/main.go
+++ day13/main.go
@@ -17,81 +17,53 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	layers := make(map[int]*layer)
+	depthToSizes := make(map[int]int)
 	totalDepth := -1
 
 	for scanner.Scan() {
 		values := strings.Split(scanner.Text(), ": ")
 		depth, size := toInt(values[0]), toInt(values[1])
-		layers[depth] = &layer{ size, 0, 1 }
+		depthToSizes[depth] = size
 		if depth > totalDepth { totalDepth = depth }
+	}
+
+	sizes := make([]int, totalDepth + 1)
+	for depth, size := range depthToSizes {
+		sizes[depth] = size
+	}
+
+	periods := make([]int, totalDepth + 1)
+	for depth, size := range depthToSizes {
+		periods[depth] = 2 * (size - 1)
 	}
 
 	fmt.Println("--- Part One ---")
 
 	severity := 0
 
-	for depth := 0; depth <= totalDepth; depth++ {
-		if layer, found := layers[depth]; found {
-			if layer.pos == 0 {
-				severity += depth * layer.size
-			}
-		}
-
-		for _, layer := range layers {
-			layer.advance()
+	for depth, period := range periods {
+		if period != 0 && depth % period == 0 {
+			severity += depth * sizes[depth]
 		}
 	}
 
 	fmt.Println(severity)
 
-	// Reset layers.
-	for _, layer := range layers  {
-		layer.pos, layer.dir = 0, 1
-	}
-
 	fmt.Println("--- Part Two ---")
-
-	// Skew layers.
-	for depth, layer := range layers {
-		for i := 0; i < depth; i++ {
-			layer.advance()
-		}
-	}
 
 	delay := 0
 
-	for {
-		good := true
-		for _, layer := range layers {
-			if layer.pos == 0 {
-				good = false
-				break
+	loop: for {
+		for depth, period := range periods {
+			if period != 0 && (delay + depth) % period == 0 {
+				delay++
+				continue loop
 			}
 		}
-		if good {
-			break
-		}
-		for _, layer := range layers {
-			layer.advance()
-		}
-		delay++
+		break
 	}
 
 	fmt.Println(delay)
-}
-
-type layer struct {
-	size, pos, dir int
-}
-
-func (layer *layer) advance() {
-	layer.pos += layer.dir
-	if layer.dir == 1 && layer.pos == layer.size - 1 {
-		layer.dir = -1
-	} else if layer.dir == -1 && layer.pos == 0 {
-		layer.dir = 1
-	}
 }
 
 func toInt(v string) int {
